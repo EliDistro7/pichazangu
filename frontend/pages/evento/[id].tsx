88c03b22f3323e7e515fb3474b5daaa4dc12d5a0
elidistro7 <elidistro@gmail.com>
2025-03-08 15:01:07 +0300

    add views and follows

--- frontend/pages/evento/[id].tsx
+++ frontend/pages/evento/[id].tsx
@@ -170,31 +170,31 @@ const EventDetails = ({ initialEvent }) => {
 
   return (
     <>
-       <Head>
-        <title>{event.title} </title>
+      <head>
+        <title>{event.title}</title>
         <meta name="description" content={initialEvent.description} />
-        <meta property="og:title" content={`${initialEvent.title} - Event Details`} />
+        <meta property="og:title" content={`${initialEvent.title} `} />
         <meta property="og:description" content={initialEvent.description} />
-        <meta property="og:image" content={initialEvent.coverPhoto} />
+        <meta property="og:image" content={`${initialEvent.coverPhoto}`} />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content={initialEvent.coverPhoto}  />
+        <meta property="og:url" content={`https://pichazangu.store/evento/${initialEvent._id}`} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={`${initialEvent.title} `} />
+        <meta name="twitter:title" content={initialEvent.title} />
         <meta name="twitter:description" content={initialEvent.description} />
-        <meta name="twitter:image" content={initialEvent.coverPhoto} />
-      </Head>
+        <meta name="twitter:image" content={`${initialEvent.coverPhoto}`} />
+      </head>
   
-      <main className=" max-w-[1960px] p-4 px-0 mx-0">
+      <main className="max-w-[1960px] p-4 px-0 mx-0">
         <SearchEvents />
-        
-           {/* Back Button */}
-           <button
-            onClick={() => router.back()}
-            className="flex items-center space-x-2 text-gray-400 ml-3 hover:text-white mb-5 transition-colors"
-          >
-            <ArrowLeft size={20} />
-            <span>Back</span>
-          </button>
+  
+        {/* Back Button */}
+        <button
+          onClick={() => router.back()}
+          className="flex items-center space-x-2 text-gray-400 ml-3 hover:text-white mb-5 transition-colors"
+        >
+          <ArrowLeft size={20} />
+          <span>Back</span>
+        </button>
   
         {/* Cover Photo Banner */}
         <div
@@ -217,17 +217,16 @@ const EventDetails = ({ initialEvent }) => {
             </Link>
           </div>
           <div className="absolute bottom-4 left-4">
-              {/* Message Icon */}
-          <button
-            onClick={() => setIsMessageModalOpen(true)}
-            className=" p-2 bg-white/80 rounded-full cursor-pointer hover:bg-white/90 transition"
-          >
-            <MessageSquare className="w-5 h-5 text-gray-700" />
-          </button>
+            {/* Message Icon */}
+            <button
+              onClick={() => setIsMessageModalOpen(true)}
+              className="p-2 bg-white/80 rounded-full cursor-pointer hover:bg-white/90 transition"
+            >
+              <MessageSquare className="w-5 h-5 text-gray-700" />
+            </button>
           </div>
           {isAuthor && (
             <div className="absolute bottom-4 right-4">
-            
               <label className="flex items-center justify-center p-2 bg-white/80 rounded-full cursor-pointer hover:bg-white/90 transition">
                 <Camera className="w-5 h-5 text-gray-700" />
                 <input
@@ -241,12 +240,11 @@ const EventDetails = ({ initialEvent }) => {
           )}
         </div>
   
-  
         <div className="container mx-auto px-4">
           <p className="mb-4 text-lg">
             <span className="font-semibold">Author:</span> {event.author.username}
           </p>
-
+  
           {/* Tabs Section */}
           <div className="flex justify-center mb-6">
             <button
@@ -298,10 +296,7 @@ const EventDetails = ({ initialEvent }) => {
           {/* Upload Progress Bar */}
           {isUploading && (
             <div className="w-full h-1 bg-gray-200">
-              <div
-                className="h-full bg-blue-600"
-                style={{ width: `${uploadProgress}%` }}
-              ></div>
+              <div className="h-full bg-blue-600" style={{ width: `${uploadProgress}%` }}></div>
             </div>
           )}
   
@@ -310,27 +305,7 @@ const EventDetails = ({ initialEvent }) => {
             <div className="columns-1 gap-4 sm:columns-2 xl:columns-3">
               {event.imageUrls && event.imageUrls.length > 0 ? (
                 event.imageUrls.map((url, index) => (
-                  <Link
-                    key={index}
-                    href={{
-                      pathname: `/p/${index}`,
-                      query: { eventId: event._id, mediaType: "photo", photoId: index },
-                    }}
-                    shallow
-                    ref={index === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-                    className="group relative mb-5 block w-full cursor-zoom-in after:content after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-                  >
-                    <Image
-                      src={url}
-                      alt={`${event.title} image ${index + 1}`}
-                      width={720}
-                      height={480}
-                      className="object-cover w-full"
-                      style={{ transform: "translate3d(0, 0, 0)" }}
-                      placeholder="blur"
-                      blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQMAAAAl21bKAAAACAAAAEMlCAYAAAACzMAAAAEhUlEQVR4nO3BMQ0AAADCoPVP8fAAAAABJRU5ErkJggg=="
-                    />
-                  </Link>
+                  <Image key={index} src={url} alt="Event Image" width={720} height={480} className="object-cover w-full" />
                 ))
               ) : (
                 <p>No images available.</p>
@@ -342,26 +317,7 @@ const EventDetails = ({ initialEvent }) => {
             <div className="columns-1 gap-4 sm:columns-2 xl:columns-3">
               {event.videoUrls && event.videoUrls.length > 0 ? (
                 event.videoUrls.map((url, index) => (
-                  <Link
-                    key={index}
-                    href={{
-                      pathname: `/p/${index}`,
-                      query: { eventId: event._id, mediaType: "video", photoId: index },
-                    }}
-                    shallow
-                    ref={index === Number(lastViewedPhoto) ? lastViewedPhotoRef : null}
-                    className="group relative mb-5 block w-full cursor-zoom-in after:content after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight"
-                  >
-                    <video
-                      src={url}
-                      className="object-cover w-full"
-                      style={{ transform: "translate3d(0, 0, 0)" }}
-                      controls={false}
-                      muted
-                      loop
-                      autoPlay
-                    />
-                  </Link>
+                  <video key={index} src={url} className="object-cover w-full" controls muted loop autoPlay />
                 ))
               ) : (
                 <p>No videos available.</p>
@@ -369,9 +325,9 @@ const EventDetails = ({ initialEvent }) => {
             </div>
           )}
         </div>
-
-          {/* Message Modal */}
-          {isMessageModalOpen && (
+  
+        {/* Message Modal */}
+        {isMessageModalOpen && (
           <div className="fixed inset-0 bg-black bg-opacity-60 backdrop-blur-lg flex items-center justify-center z-50">
             <div className="relative bg-gray-900 text-white rounded-lg shadow-lg p-6 max-w-sm w-full">
               <button
@@ -380,12 +336,12 @@ const EventDetails = ({ initialEvent }) => {
               >
                 <X className="text-white" size={20} />
               </button>
-              <MessageForm eventId={initialEvent._id} userId={`${initialEvent.author.userId}`}/>
+              <MessageForm eventId={initialEvent._id} userId={`${initialEvent.author.userId}`} />
             </div>
           </div>
         )}
       </main>
-      
+  
       {/* Global Loading UI for media route changes */}
       {isMediaLoading && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
@@ -396,6 +352,7 @@ const EventDetails = ({ initialEvent }) => {
       <ToastContainer />
     </>
   );
+  
 };
   
 export async function getServerSideProps({ params }) {
