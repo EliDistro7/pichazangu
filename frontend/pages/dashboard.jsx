ccdd6298d16a36be9ca673270727e469337e3a94
elidistro7 <elidistro@gmail.com>
2025-03-14 20:01:07 +0300

    add views and follows

--- frontend/pages/dashboard.jsx
+++ frontend/pages/dashboard.jsx
@@ -32,7 +32,7 @@ const formatStorageSize = (sizeInBytes) => {
   } else if (sizeInBytes < 1024 * 1024) {
     return `${(sizeInBytes / 1024).toFixed(2)} KB`; // Kilobytes
   } else if (sizeInBytes < 1024 * 1024 * 1024) {
-    return `${(sizeInBytes / (1024 * 1024)).toFixed(2)} MB`; // Megabytes
+    return `${(sizeInBytes / (1024 * 1024)).toFixed(0)} MB`; // Megabytes
   } else if (sizeInBytes < 1024 * 1024 * 1024 * 1024) {
     return `${(sizeInBytes / (1024 * 1024 * 1024)).toFixed(2)} GB`; // Gigabytes
   } else {
