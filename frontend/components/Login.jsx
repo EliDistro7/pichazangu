b910c92ef360112caea9efecd20f1d9a72036733
elidistro7 <elidistro@gmail.com>
2025-04-11 03:12:26 +0300

    solved

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -4,7 +4,7 @@ import { toast, ToastContainer } from "react-toastify";
 import { useRouter } from "next/router";
 import "react-toastify/dist/ReactToastify.css";
 import {setSecureCookie} from "../hooks/useUser";
-import TurnstileIntegration from './turnstile/integration';
+import TurnstileIntegration from './Integration';
 
 
 const Login = () => {
